c85900b203db56aee591d165ac5fb3503e79856f
markb <markbuchholz8@gmail.com>
2023-03-23 20:09:15 -0600

    selectors

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//first i will use the queryselector to grab my logo tag
+let title_logo = document.querySelector(`#title_logo`);
+//second i will be grabbing my queryselectall to grab all my classes in my main
+let main_p_tags = document.querySelectorAll(`.div_p`);
+//third i will be grabbing my footer id tag
+let footer_info = document.getElementById(`footer_info`);
+//and last but not least i will be grabbing the rest of my header classes
+let header_p = document.getElementsByClassName(`header_p`)
