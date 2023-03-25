78076e6882fa3c5c57294dc2ea17f3940f0b7d9e
markb <markbuchholz8@gmail.com>
2023-03-24 20:28:03 -0600

    finished insertAdjacentHTML

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@ let title_logo = document.querySelector(`#title_logo`);
 //second i will be grabbing my queryselectall to grab all my classes in my main
 let main_p_tags = document.querySelectorAll(`.div_p`);
 //third i will be grabbing my footer id tag
-let footer_info = document.getElementById(`footer_info`);
-//and last but not least i will be grabbing the rest of my header classes
 let header_p = document.getElementsByClassName(`header_p`)
+//and last but not least i will be grabbing the rest of my header classes
+let footer_info = document.getElementById(`footer_info`);
+//i will be changing the elements style
+title_logo [`style`][`color`] = `red`;
+//now i will add content
+main_p_tags[`innerHTML`] = `this is new content for my main p tags`;
+//i will be adding new nested tags
+footer_info.insertAdjacentHTML(`afterbegin` , `<p>this is a new p tag</P>`)
+//adding a new tag using insertAdjacentHTML to put it at the before end 
+footer_info.insertAdjacentHTML(`beforebegin`,`<h2> This is a new title </h2>`);
